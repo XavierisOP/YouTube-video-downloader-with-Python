e1d1fe9818d3dd2e666d4a2fee73068e482f6bf9
Xavier <71417757+XavierisOP@users.noreply.github.com>
2022-10-11 12:51:00 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+## **Python script to downlaod YouTube videos From Playlist, Channel, Bulk, Single, Recently Uploaded etc**
+Python script to downlaod YouTube videos From Playlist, Channel, Bulk, Single, Recently Uploaded etc
+<p align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Lobster&size=23&duration=1000&pause=1000&color=19F2FF&background=FFFFFF00&center=true&vCenter=true&width=250&lines=YouTube+Videos;Downloader;Bulk+and+Single+Videos;From+PlayList+or+a+Channel" alt="Typing SVG" /></a>
+</p>
+<details>
+ <summary>Advantages</summary>
+  - It can Download a complete Channel which has tons of vidos.<br />
+  - If it crashed then it will automatically skip downlaoded video after restart.<br />
+  - Exceptions are handled
+ </details>
+
+
+# Features😶‍🌫️
+
+    1. Interactive with Menu Options
+    2. Bulk & Single Download
+    3. From a Specific Channel Videos
+    4. From any PlayList
+    5. Recent Videos
+
+### Dependancies: 
+   [pytube](https://github.com/pytube/pytube)
+
+
+### Quick Start🐿️ :
+  Download and install [Python](https://www.python.org/downloads/) and [pip](https://pip.pypa.io/en/stable/installing/) if necessary.<br />
+  [Downlaod](https://github.com/XavierisOP/YouTube-video-downloader-with-Python) the Reposetory or <br />
+  <code>git clone https://github.com/XavierisOP/YouTube-video-downloader-with-Python</code>
+  - Open a terminal and run <code>pip install pytube </code>
+  - Open a terminal and run <code>python main.py </code>
---
+++ main.py
@@ -0,0 +1,134 @@
+import pytube
+from pytube import Channel
+from pytube import YouTube
+from pytube import exceptions
+from pytube import Playlist
+from pytube.cli import on_progress
+# channel_link = "https://www.youtube.com/channel/UCnQucpXURwXBgeAmwn3QF2g"
+# channel = 'https://www.youtube.com/c/KaanPhodMusic/videos'
+
+fuchsia = '\033[38;2;255;00;255m'
+# color as hex #FF00FF
+reset_color = '\033[39m'
+
+def progress(streams, chunk: bytes, bytes_remaining: int):
+    contentsize = video.filesize
+    size = contentsize - bytes_remaining
+
+    print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
+    '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
+
+while True:
+    directory = input("Enter a Directiory where all video will be download! » ")
+    menu = int(input("""
+CHOOSE THE OPTION ACCORDING TO YOUR NEED - 
+[01] » Downlaod a Single Video
+[02] » Download From PlayList
+[03] » Download From a Channel
+[INPUT] Enter your choice! » """))
+
+    # SINGLE VIDEO DOWNLOAD
+    if menu == 1:
+        url = (input("""
+Download for a Single Video - 
+[INPUT] Enter a video URL! » """))
+        try:
+            yt = YouTube(url,on_progress_callback=on_progress)
+            print(f'{yt.title} Video Downloading...')
+            yt.streams.get_highest_resolution().download(directory)
+            print('Video Downloaded...')
+        except exceptions.VideoPrivate:
+            print('Video is Private') 
+        except exceptions.VideoRegionBlocked:
+            print('Video is blocked')
+            
+        except exceptions.VideoUnavailable:
+            print('Video is Unavailable')
+
+
+    # PLAYLIST DOWNLOAD FULL & Recent
+    elif menu == 2:
+        playlist_url = (input("""
+Download From a PlayList - 
+[INPUT] Enter a Playlist URL! » """))
+        playlist = Playlist(playlist_url)
+        print("Total Videos", len(playlist.video_urls))
+
+        pMenu = int(input("""
+Download PlayList Bulk or Selective - 
+[01] » Downlaod all Videos From Playlist
+[02] » Downlaod Recently Uploaded Videos From Playlist
+[INPUT] Enter your choice! » """))
+        
+        
+        if pMenu  == 1:
+            
+            for video in playlist.videos:
+                try:
+                    video.register_on_progress_callback(on_progress)
+                    print(f'{video.title} is Downlaoding.')
+                    video.streams.get_highest_resolution().download(directory)
+                    print(f'{video.title} is Downloaded')
+                except VideoUnavailable:
+                    print(f'{video.title} is unavaialable, skipping.')
+        elif pMenu  == 2:
+            nChoise = int(input("""
+Download Recent Video From PlayList -
+[INPUT] Enter No. of Recent Videos You want to Download..! » """))
+
+            for video in playlist.videos[:nChoise]:
+                try:
+                    video.register_on_progress_callback(on_progress)
+                    print(f'{video.title} is Downlaoding.')
+                    video.streams.get_highest_resolution().download(directory)
+                    print(f'{video.title} is Downloaded')
+                except VideoUnavailable:
+                    print(f'{video.title} is unavaialable, skipping.')
+        else:
+            print(f'INVALID INPUT') 
+            pass     
+
+
+
+    elif menu == 3:
+        channel_link = (input("""
+Download From a Channel - 
+[INPUT] Enter a Channel URL! » """))
+        channel = Channel(channel_link)
+        print(channel.channel_name)
+        print(channel.channel_url)
+        print("Total Videos", len(channel.video_urls))
+        choise = int(input("""
+Download From Channel Bulk or Selective - 
+[01] » Downlaod all Videos From Channel
+[02] » Downlaod Recently Uploaded Videos From Channel
+[INPUT] Enter your choice! » """))
+        if choise == 1:
+            
+            for video in channel.videos:
+                try:
+                    print(f'{video.title} is Downlaoding.')
+                    video.register_on_progress_callback(on_progress)
+                    video.streams.get_highest_resolution().download(directory)
+                except VideoUnavailable:
+                    print(f'{video.title} is unavaialable, skipping.')
+        elif choise == 2:
+            recentChoise = int(input("""
+Download Recent Video From Channel -
+[INPUT] Enter No. of Recent Videos You want to Download..! » """))
+
+            for video in channel.videos[:recentChoise]:
+                try:
+                    print(f'{video.title} is Downlaoding.')
+                    video.register_on_progress_callback(on_progress)
+                    video.streams.get_highest_resolution().download(directory)
+                    print(f'{video.title} is Downloaded')
+                except VideoUnavailable:
+                    print(f'{video.title} is unavaialable, skipping.')
+        else:
+            print(f'INVALID INPUT')   
+            pass         
+
+    else:
+        print(f'INVALID INPUT') 
+        pass
